addf17221281026c922d3e11dffd01e9ff454952
nechek805 <chekmakkonstantin@gmail.com>
2026-04-24 21:27:44 +0300

    clear main file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
-from src.task.router import router as task_router
-
 
 app = FastAPI(title="Very loaded API")
-
-app.include_router(task_router)
 
 @app.get("/")
 def main():
