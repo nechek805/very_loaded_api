3b3e1741991607ff30e5af4aa0b35139e93549b0
nechek805 <chekmakkonstantin@gmail.com>
2026-04-24 21:09:23 +0300

    remove task models from env.py

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,6 @@ from alembic import context
 
 from src.core.config import DATABASE_URL
 from src.core.database import Base
-from src.task import models #noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
